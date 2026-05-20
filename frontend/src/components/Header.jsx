62a80b1a73af329d2179c8dc1ee449037d25acfb
MasaSaranovic <masasaranovic@gmail.com>
2026-05-20 23:03:57 +0200

    Vežbe 8 - Frontend Auth

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,39 @@
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png'
 import { LinkContainer } from 'react-router-bootstrap'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 const Header = () => {
+
     const { cartItems } = useSelector((state) => state.cart);
+    const { userInfo } = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (err) {
+            console.error('Logout failed:', err);
+        }
+
+    }
+
     return (
         <header>
             <Navbar bg="primary" variant="dark" expand="md" collapseOnSelect>
                 <Container>
                     <LinkContainer to="/">
                         <Navbar.Brand>
-                            <img src={logo} alt="FTN Skriptarnica" width="30"
-                                height="30" className="d-inline-block align-top me-2" />
+                            <img src={logo} alt="FTN Skriptarnica" width="30" height="30" className="d-inline-block align-top me-2" />
                             <span className="fw-semibold">Skriptarnica </span>
                             Fakulteta tehničkih nauka u Novom Sadu
                         </Navbar.Brand>
@@ -23,18 +44,23 @@ const Header = () => {
                             <LinkContainer to="/cart">
                                 <Nav.Link><FaShoppingCart /> Korpa
                                     {cartItems.length > 0 && (
-                                        <Badge pill bg="success" style={{
-                                            marginLeft: '5px'
-                                        }}>
-                                            {cartItems.reduce((a, c) => a +
-                                                c.qty, 0)}
+                                        <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                                            {cartItems.reduce((a, c) => a + c.qty, 0)}
                                         </Badge>
                                     )}
                                 </Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link><FaUser /> Prijava</Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id="username">
+                                    <LinkContainer to="/profile">
+                                        <NavDropdown.Item>Profil</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Odjava</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link><FaUser /> Prijava</Nav.Link>
+                                </LinkContainer>)}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -42,4 +68,5 @@ const Header = () => {
         </header>
     )
 }
+
 export default Header
