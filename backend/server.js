02405049528b5b03741789c9cf206ff509d4fb79
MasaSaranovic <masasaranovic@gmail.com>
2026-05-21 02:38:18 +0200

    Vežbe 8 - SamostalniNastavak

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import { notFound, errorHandler } from './middleware/errorHandler.js'
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js';
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -29,6 +30,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
